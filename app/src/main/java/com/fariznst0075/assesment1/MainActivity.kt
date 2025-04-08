a9377604a417caf7793dd5eb4d94e7a9bf32b450
Fariz Rahman Nst <farizrahmannst14@gmail.com>
2025-04-08 13:02:08 +0700

    Menambahkan input dan translate

--- app/src/main/java/com/fariznst0075/assesment1/MainActivity.kt
+++ app/src/main/java/com/fariznst0075/assesment1/MainActivity.kt
@@ -1,28 +1,10 @@
 package com.fariznst0075.assesment1
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.fariznst0075.assesment1.ui.screen.MainScreen
 import com.fariznst0075.assesment1.ui.theme.Assesment1Theme
 
 class MainActivity : ComponentActivity() {
@@ -34,55 +16,5 @@ class MainActivity : ComponentActivity() {
                 MainScreen()
             }
         }
-    }
-}
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen(){
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(text = stringResource(id = R.string.app_name)) },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-            )
-            )
-        }
-    ) { padding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(padding),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-
-            Image(
-                painter = painterResource(id = R.drawable.logo_app),
-                contentDescription = stringResource(id = R.string.logo_desc),
-                modifier = Modifier
-                    .padding(top = 16.dp)
-                    .height(120.dp)
-            )
-
-
-        }
-    }
-}
-
-@Composable
-fun ScreenContent(modifier: Modifier = Modifier){
-    Text(
-        text = "Hello Android",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
-@Composable
-fun MainScreenPreview() {
-    Assesment1Theme {
-        MainScreen()
     }
 }
--- app/src/main/java/com/fariznst0075/assesment1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/fariznst0075/assesment1/ui/screen/MainScreen.kt
@@ -2,21 +2,31 @@ package com.fariznst0075.assesment1.ui.screen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.fariznst0075.assesment1.R
@@ -58,10 +68,43 @@ fun MainScreen(){
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier){
-    Text(
-        text = "Hello Android",
-        modifier = modifier
-    )
+    var suhu1 by remember { mutableStateOf("") }
+    var suhu2 by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.suhu_intro),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = suhu1,
+            onValueChange = { suhu1 = it},
+            label = { Text(text = stringResource(R.string.suhu1)) },
+            trailingIcon = { Text(text = "°") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = suhu2,
+            onValueChange = { suhu2 = it},
+            label = { Text(text = stringResource(R.string.suhu2)) },
+            trailingIcon = { Text(text = "°") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
 
 @Preview(showBackground = true)
